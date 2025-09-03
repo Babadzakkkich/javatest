8eafbd82872b730971de6103985d042457467653
Babadzakkkich <Bogdamirkobz@gmail.com>
2025-09-03 17:20:16 +0300

    Third commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
         System.out.println("Hello and welcome!");
+        System.out.println("HELLO and welcome!");
     }
 }
